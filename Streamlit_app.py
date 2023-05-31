00ea663aaba215349f0a4c0dfb510fe7142473c2
ganeshsalunkhe99 <135030687+ganeshsalunkhe99@users.noreply.github.com>
2023-05-31 13:22:16 +0530

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -9,6 +9,8 @@ streamlit.text('🥑 Avocado test')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
+streamlit.header("Fruityvice Fruit Advice!")
+
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
